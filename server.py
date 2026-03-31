68709c56f07603769cadefc6f35eb13fa04aa213
12213495 <119958960+12213495@users.noreply.github.com>
2026-03-31 13:24:50 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const taskRoutes = require("./routes/taskRoutes");
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+// Routes
+app.use("/api/tasks", taskRoutes);
+
+// DB Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
